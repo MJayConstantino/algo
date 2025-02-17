d03cebfa94263d5009daa216d862cd81534d0dbb
MJayConstantino <mjconstantino12345@gmail.com>
2025-02-17 12:37:18 +0800

    added add first and last logic

--- Lab 2/deque.py
+++ Lab 2/deque.py
@@ -4,7 +4,6 @@
 # For example:
 # d = Deque[int]()
 #
-Item = type('Item')
 
 class Node:
     def __init__(self, item):
@@ -16,39 +15,63 @@ class Node:
 class Deque[Item]:
     # construct an empty deque
     def __init__(self):
-        self.first = None
-        self.last = None
+        self.first: Node = None
+        self.last: Node = None
         self.size = 0
 
     # is the deque empty?
     def is_empty(self) -> bool:
-        return len(self.size) == 0
+        return self.size == 0
 
     # return the number of items on the deque
     def size(self) -> int:
-        return len(self.size)
+        return self.size
 
     # add the item to the front
     def add_first(self, item: Item) -> None:
         if item is None:
             raise ValueError("Item should not be none!")
+        new_node = Node(item)
+        if self.is_empty:
+            self.first = new_node
+            self.last = new_node
+        else:
+            new_node.next = self.first
+            self.first.prev = new_node
+            self.first = new_node
+        self.size += 1
 
     # add the item to the back
     def add_last(self, item: Item) -> None:
         if item is None:
             raise ValueError("Item should not be none!")
+        new_node = Node(item)
+        if self.is_empty:
+            self.first = new_node
+            self.last = new_node
+        else:
+            new_node.prev = self.last
+            self.last.next = new_node
+            self.last = new_node
+        self.size += 1
 
     # remove and return the item from the front
     def remove_first(self) -> Item:
         if self.is_empty:
             raise IndexError("Deque is empty!")
+        current_first = self.first
+        self.first = current_first.next
+        self.size -= 1
+        return current_first.item
 
     # remove and return the item from the back
     def remove_last(self) -> Item:
         if self.is_empty:
             raise IndexError("Deque is empty!")
-        else:
-            self.r
+        current_last = self.last
+        self.last = current_last.prev
+        self.size -= 1
+        return current_last.item
 
     def __iter__(self):
         pass
@@ -61,8 +84,11 @@ class Deque[Item]:
     # unit testing (required)
     @staticmethod
     def main():
-        pass
-
+        d = Deque()
+        d.add_first(1)
+        d.add_last(2)
+        print(d.is_empty())
+        print(d.size)
 
 if __name__ == "__main__":
     Deque.main()
