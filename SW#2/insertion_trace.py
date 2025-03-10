6424d9c7fb25e6566c435686f62204fe803ac4a7
MJayConstantino <mjconstantino12345@gmail.com>
2025-03-10 11:23:21 +0800

    fixed stuff

--- SW#2/insertion_trace.py
+++ SW#2/insertion_trace.py
@@ -1,15 +1,1 @@
-def selection_sort(string: str):
-    string = list(string)
-    result = []
-    for i in range(len(string)):
-        min_index = i
-        for j in range(i + 1, len(string)):
-            if string[j] < string[min_index]:
-                min_index = j
-        string[i], string[min_index] = string[min_index], string[i]
-        result.append(f"{i:<2} {min_index:<2} {' '.join(string)}")
-    return "\n".join(result)
 
-if __name__ == '__main__':
-    import sys
-    print(selection_sort(sys.argv[1]))
