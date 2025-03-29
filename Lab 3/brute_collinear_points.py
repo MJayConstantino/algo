f16c1de428a7e217ddf98a2dfc1ea55bf48c6eaf
MJayConstantino <mjconstantino12345@gmail.com>
2025-03-30 01:07:08 +0800

    Update brute_collinear_points.py

--- Lab 3/brute_collinear_points.py
+++ Lab 3/brute_collinear_points.py
@@ -15,15 +15,14 @@ class BruteCollinearPoints:
             for j in range(i + 1, len(points)):
                 if points[i] == points[j]:
                     raise ValueError("The list of points contains a repeated point")
-                
         for i in range(len(points)):
-            for j in range(i+1 , len(points)):
-                for k in range(j+1, len(points)):
-                    for l in range(k+1, len(points)):
+            for j in range(i + 1, len(points)):
+                for k in range(j + 1, len(points)):
+                    for l in range(k + 1, len(points)):
                         p, q, r, s = points[i], points[j], points[k], points[l]
                         if p.slope_to(q) == p.slope_to(r) == p.slope_to(s):
-                            self._segments.append(LineSegment(p, s))
-        
+                            endpoints = sorted([p, q, r, s])
+                            self._segments.append(LineSegment(endpoints[0], endpoints[-1]))
 
     def number_of_segments(self) -> int:
         return len(self._segments)
